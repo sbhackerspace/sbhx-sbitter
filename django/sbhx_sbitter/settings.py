3b47b273398caa0502f6e201db5328fe19066906
Steve Phillips <elimisteve@gmail.com>
2011-11-25 19:01:53 -0800

    Relativized settings.py; should now work on any system. (Yay!)

--- django/sbhx_sbitter/settings.py
+++ django/sbhx_sbitter/settings.py
@@ -1,7 +1,11 @@
 # Django settings for sbhx_sbitter project.
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+THIS_DIR = os.path.dirname(__file__)
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -12,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/steve/django_projects/sbhx_sbitter/sql.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(THIS_DIR, 'sql.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -106,7 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/steve/django_projects/sbhx_sbitter/templates'
+    os.path.join(THIS_DIR, 'templates')
 )
 
 INSTALLED_APPS = (
